d6af418e3319541edb124886cfc5aa58d0dd01b7
Juliia Lurieva <Juliia_Lurieva@epam.com>
2016-11-10 16:08:18 +0200

    feature-45: Add favicon, base layuot, routing

--- src2/src/app/app.routes.js
+++ src2/src/app/app.routes.js
@@ -1,16 +1,17 @@
-function routeConfig ($stateProvider, $urlRouterProvider) {
+function routeConfig ($stateProvider, $urlRouterProvider, $locationProvider) {
     'ngInject';
 
     $stateProvider
-        .state('home', {
-            url: '/',
-            template: require('./screens/home.html')
-        })
         .state('signin', {
-            url: '/signin',
+            url: '/',
             template: require('./screens/signin.html')
+        })
+        .state('home', {
+            url: '/home',
+            template: require('./screens/home.html')
         });
 
+    $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/');
 }
 
