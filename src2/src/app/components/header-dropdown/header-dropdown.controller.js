0f0e8924a43c284176e90b06c1f5cada3aca536d
bjuly <yuliya_astakhova@epam.com>
2016-11-11 12:49:55 +0200

    feature-48: header, footer refactoring

--- src2/src/app/components/header-dropdown/header-dropdown.controller.js
+++
@@ -1,9 +0,0 @@
-class HeaderDropdownController {
-    constructor() {
-    }
-
-    $onInit() {
-    }
-}
-
-export default HeaderDropdownController;
--- src2/src/app/components/header-dropdown/header-dropdown.module.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import HeaderDropdownComponent from './header-dropdown.component.js';
-
-const headerDropdownComponent = angular
-    .module('headerDropdownComponent', [])
-    .component('headerDropdown', HeaderDropdownComponent)
-    .name;
-
-export default headerDropdownComponent;
