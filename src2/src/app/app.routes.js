5071f21fca0e5aaa4089536b560f3de55d237989
Natalia Kozbina <Natalia_Kozbina@epam.com>
2016-11-14 12:35:08 +0200

    feature-46: create routing between page login and reristration

--- src2/src/app/app.routes.js
+++ src2/src/app/app.routes.js
@@ -9,7 +9,12 @@ function routeConfig ($stateProvider, $urlRouterProvider, $locationProvider) {
         .state('home', {
             url: '/home',
             template: require('./screens/home.html')
+        })
+        .state('registration', {
+            url: '/registration',
+            template: require('./screens/signin.html')
         });
+
 
     $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/');
