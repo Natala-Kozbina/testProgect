1dfa2e9a5d7bd6d810585f4165302902f8f19c56
Natalia Kozbina <Natalia_Kozbina@epam.com>
2016-11-16 13:35:14 +0200

    feature-39: remove Signin-notification after change input

--- src2/src/app/containers/login-form/login-form.controller.js
+++ src2/src/app/containers/login-form/login-form.controller.js
@@ -4,37 +4,41 @@ class LoginFormController {
         this.state = $state;
         this.loginService = loginService;
         this.$log = $log;
+        this.user = {};
     }
 
     checkSignIn (ngModel) {
         let email = ngModel.email;
         let password = ngModel.password;
 
-        let data = {
+        this.user = {
             'email' : email,
             'password' : password
         }
-        console.log(data);
-        this.loginHandler(data);
+
+        this.loginHandler();
 
         // this.loginService
-        //     .checkLoginForm(email, password)
+        //     .checkLoginForm(user)
         //     .then(this.loginHandler.bind(this))
         //     .catch(this.errorHandler.bind(this));
         // console.log('ngModel => ',  ngModel);
     }
 
-    loginHandler(data) {
+    loginHandler() {
+        console.log(this.user);
 
-        let user = {
+        let userFromServer = {
             'email' : "www@www.www",
             'password' : "xxx"
         };
 
-        if(user.email === data.email && user.password === data.password) {
+        if(userFromServer.email === this.user.email && userFromServer.password === this.user.password) {
             this.state.go('home');
+        } else if (userFromServer.email != this.user.email) {
+            return this.errorEmail = true;
         } else {
-            return this.error = true;
+            return this.errorPassword = true;
         }
     }
 
@@ -44,6 +48,16 @@ class LoginFormController {
 
     redirect() {
         this.state.go("registration");
+    }
+
+    changeEmail() {
+        console.log('changeEmail');
+        return this.errorEmail = false;
+    }
+
+    changePassword() {
+        console.log('changePassword');
+        return this.errorPassword = false;
     }
 }
 
--- src2/src/app/directives/form-validator/text-only.directive.js
+++ src2/src/app/directives/form-validator/text-only.directive.js
@@ -7,7 +7,7 @@ class TextOnly {
 
         $ctrl.$validators.textOnly = function(modelValue) {
             var length = $attrs.minLength;
-            var reg = new RegExp("^[A-ZА-Я][a-zа-я]{" + length + ",}$");
+            var reg = new RegExp("^[A-ZА-Яa-zа-я][a-zа-я]{" + length + ",}$");
             if (reg.test(modelValue)) {
                 return true;
             } else {
